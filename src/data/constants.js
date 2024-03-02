bfd9526b7bfd14ca66ed63fa1f51319b10b469fd
Angel Ruben Acosta Merino <114235440+AngelRAMUPPE@users.noreply.github.com>
2024-03-02 04:07:19 +0000

    setup-react

---
+++ src/data/constants.js
@@ -0,0 +1,184 @@
+
+export const Bio = {
+  name: "Angel Ruben Acosta Merino",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/AngelRAMUPPE",
+  resume:
+    "https://drive.google.com/file/d/1QiLYBfznMcWXi2t5LLKbF2LH7QVwvtKc/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/angelrubenacostamerino/",
+  insta: "https://instagram.com/angelruben_31",
+  facebook: "https://www.facebook.com/profile.php?id=100010657803536",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+// export const experiences = [
+//   {
+//     id: 0,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/neurobit.jpeg?alt=media&token=1cacbb7b-e5ea-4efb-9b3d-f5a31b9564ad",
+//     role: "",
+//     company: "",
+//     date: "",
+//     desc: "",
+//     skills: [],
+//     doc: "",
+//   },
+// ];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portafolio-19d21.appspot.com/o/UPPE.jpeg?alt=media&token=95bed092-d361-4b6c-a416-b0aefe365b89",
+    school: "Universidad Politecnica de Penjamo",
+    date: "Sep 2022 - Now",
+    grade: " 9.3 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Software Engineering at Universidad Politecnica de Penjamo. I have completed 5 semesters and have a CGPA of 9.3. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor in Software Engineering",
+  },
+  {
+    id: 1,
+    img: "https://firebasestorage.googleapis.com/v0/b/portafolio-19d21.appspot.com/o/CBTIS.jpeg?alt=media&token=3293ddf3-a921-4f4d-aa6c-f09d23ca04a7",
+    school: "Centro De Bachillerato Tecnológico Industrial Y De Servicios Nº171",
+    date: "Sep 2019 - Jun 2022",
+    grade: "90%",
+    desc: "I completed my high school education at CBTIS 171, where I studied the basic into the world of Programming.",
+    degree: "Programming Technician",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "AppGrow",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "A Project that involves aspects of Hardware and software, the capacity of our meter offers precise measurements which is crucial to guarantee the quality of our plants and to maintain optimal environments. We are confident that our technology will allow us to maintain high levels of quality and efficiency.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portafolio-19d21.appspot.com/o/Screenshot%20(38).png?alt=media&token=5b37d988-04c2-423d-ad8e-176754cf6dd5",
+    tags: [
+      "React Js",
+      "Node Js",
+      "PHP",
+      "MYSQL",
+      "Arduino",
+    ],
+    category: "web app",
+    github: "https://github.com/AngelRAMUPPE/FinalProject_WebProgramming",
+    webapp: "https://angelramuppe.github.io/FinalProject_WebProgramming/",
+  },
+];
+
